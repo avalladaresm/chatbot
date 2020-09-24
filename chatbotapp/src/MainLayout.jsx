9b0460c496803c105170fbc0c181cfc1f5232a42
Alejandro Valladares <avalladaresm@unitec.edu>
2020-09-24 12:23:03 -0600

    first commit

---
+++ chatbotapp/src/MainLayout.jsx
@@ -0,0 +1,58 @@
+import 'antd/dist/antd.css';
+import 'react-chat-elements/dist/main.css';
+import React from 'react'
+import { Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+import {TrelloChannel} from './TrelloChannel'
+import {MeetingsChannel} from './MeetingsChannel'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+const { Header, Content, Sider } = Layout;
+function BasicLayout(props) {
+  return (
+		<Router>
+			<Layout style={{height:'100vh'}}>
+				<Sider
+					breakpoint='lg'
+					collapsedWidth='0'
+					onBreakpoint={broken => {
+						console.log(broken);
+					}}
+					onCollapse={(collapsed, type) => {
+						console.log(collapsed, type);
+					}}
+				>
+					<div className='logo' />
+					<Menu theme='dark' mode='inline' defaultSelectedKeys={['4']}>
+						<Menu.Item key='1' icon={<UserOutlined />}>
+							<Link to='/trello'>Trello chat</Link>
+						</Menu.Item>
+						<Menu.Item key='2' icon={<VideoCameraOutlined />}>
+							<Link to='/meetings'>Meetings chat</Link>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout>
+					<Header className='site-layout-sub-header-background' style={{ padding: 0 }} />
+					<Content style={{ margin: '24px 16px 0' }}>
+						<Switch>
+							<Route path='/trello'>
+								<TrelloChannel />
+							</Route>
+							<Route path='/meetings'>
+								<MeetingsChannel />
+							</Route>
+						</Switch>
+					</Content>
+				</Layout>
+			</Layout>
+		</Router>
+  );
+}
+
+export default BasicLayout;
---
+++ chatbotapp/src/TrelloChannel.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { MessageList } from 'react-chat-elements'
+import { Input, Button } from 'antd'
+
+export const TrelloChannel = () => {
+  const [message, setMessage] = useState();
+  const [dataSource, setDataSoure] = useState([{
+    position: 'right',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  {
+    position: 'right',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  {
+    position: 'left',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  {
+    position: 'right',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  }]);
+  
+  useEffect(() => {
+    
+
+  }, [dataSource])
+  
+  const createNewMessage = (message) => {
+    let m = {position:'right', type:'text', text:message, date:new Date()}
+    setDataSoure([...dataSource, m]);
+    setMessage('')
+  }
+
+  return (
+    <>
+      <MessageList
+        className='message-list'
+        lockable={true}
+        toBottomHeight={'100%'}
+        dataSource={dataSource}
+      />
+      <Input value={message} onChange={e => setMessage(e.target.value)} addonAfter={<Button onClick={() => createNewMessage(message)}>Send</Button>} />
+    </>
+  )
+}
